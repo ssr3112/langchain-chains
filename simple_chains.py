fb457150be6a09f7eaa87d42cf3ec0e7e72af6bd
ssr3112 <159468932+ssr3112@users.noreply.github.com>
2025-05-09 01:14:02 +0530

    Add files via upload

---
+++ simple_chains.py
@@ -0,0 +1,19 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt= PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+parser=StrOutputParser()
+
+chain=prompt| model|parser
+result=chain.invoke({'topic':'cricket'})
+print(result)
